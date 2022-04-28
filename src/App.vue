7ea504fd4215198c9895f5983da739024356e005
josue ubilla <120-josue.ubilla@users.noreply.repositorios.ctdesarrollo.org>
2022-04-28 08:45:03 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,117 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <v-container fill-height fluid justify-center class="d-flex flex-column" v-if="$store.state.data_base_name==null">
+        <div class="mb-2 d-flex flex-column">
+          <v-icon :color="$store.state.data_base_name ? 'green':'red'" class="mb-2">
+            fas fa-database
+          </v-icon>
+          <p class="ma-0 green--text" v-if="$store.state.data_base_name">
+            Database connected
+          </p>
+          <p class="ma-0 red--text" v-else>
+            No database connected
+          </p>
+        </div>
+        <div>
+          <v-text-field v-model="data_base_name" outlined dense append-outer-icon="fas fa-plug" @click:append-outer="connect_database"></v-text-field>
+        </div>
+      </v-container>
+      <v-container v-else>
+        <v-text-field v-model="search_service" clearable prepend-inner-icon="fab fa-searchengin" outlined dense></v-text-field>
+        <v-data-table :headers="headers" :items="services" :search="search_service" :options.sync="options" 
+          :loading="loading" class="elevation-1">
+          <template v-slot:[`item.activate`]="{ item }">
+            <div v-if="item.activate">
+              <v-btn icon @click="change_service(item)" color="green">
+                <v-icon>
+                  fas fa-check-circle
+                </v-icon>
+              </v-btn>
+            </div>
+            <div v-else>
+              <v-btn icon @click="change_service(item)" color="red">
+                <v-icon>
+                  fas fa-times-circle
+                </v-icon>
+              </v-btn>
+            </div>
+          </template>
+          </v-data-table>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+import axios from 'axios'
+  export default {
+    data () {
+      return {
+        showMore:true,
+        totalServices: 0,
+        services: [],
+        loading: true,
+        search_service:'',
+        options: {},
+        data_base_name:'',
+        headers: [
+          {
+            text: 'Id',
+            align: 'start',
+            sortable: false,
+            value: 'id',
+          },
+          { text: 'Name', value: 'name' },
+          { text: 'Activate', value: 'activate' },
+        ],
+      }
+    },
+    watch: {
+      options: {
+        handler () {
+          this.getDataFromApi()
+        },
+        deep: true,
+      }
+    },
+    methods: {
+      getDataFromApi () {
+        this.loading = true
+        var options = JSON.stringify(this.options)
+        axios.get(`http://ubuntu20-04.ctdesarrollo.org:3002/api/get_all_result/2/${options}`).then((response)=>{
+          this.services = response.data
+          this.totalServices = response.data.length
+          this.loading = false
+        })
+      },
+      change_service(item){
+        var body = new Object();
+        var url = '';
+        if(item.activate){
+          url = "remove_mdl_external_services_functions";
+        }else{
+          url = "add_mdl_external_services_functions";
+        }
+        body.functionname = item.name
+        body.externalserviceid = 2;
+        axios.post(`http://ubuntu20-04.ctdesarrollo.org:3002/api/${url}`,body).then((response)=>{
+          if(response.data){
+            this.getDataFromApi()
+          }
+        });
+      },
+      async connect_database(){
+        var body = new Object();
+        body.database = this.data_base_name;
+        var response = await axios.post('http://192.168.0.79:3002/api/check_database', body);
+        console.log(response);
+        if(response.data == "connected"){
+          this.$store.commit('database_connected',this.data_base_name);
+        }else{
+          this.$store.commit('database_disconnected');
+        }
+      }
+    },
+  }
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    data_base_name:null
   },
   mutations: {
+    database_connected(state,data_base_name){
+      setTimeout(() => {
+        state.data_base_connected = true
+        state.data_base_name = data_base_name
+        localStorage.setItem('database_name',data_base_name);
+      }, 2000);
+    },
+    database_disconnected(state){
+      state.data_base_connected = false
+    }
   },
   actions: {
   },
